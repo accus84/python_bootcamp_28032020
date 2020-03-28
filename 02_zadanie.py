922453ab219a7a3815fcd8d260e8b206628de2f2
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-28 13:48:22 +0100

    initial

---
+++ 02_zadanie.py
@@ -0,0 +1,25 @@
+#P= 1 / 2 * a * h
+a = 10
+h = 5
+P = 1 / 2 * a * h
+print("Pole trójkąta wynosi", P)
+
+#P = pi * r**
+pi = float(3.14)
+r = 7
+P = pi * r*r
+#może być też pi * r ** 2 bo ** oznacza "do Potęgi"
+print("Pole koła wynosi", P)
+
+#P = (a + b) / 2 * h
+a = 3
+b = 9
+h = 6.5
+P = (a + b) / 2 * h
+print("Pole trapezu wynosi", P)
+
+#V = 4 / 3 * pi * R*R*R
+pi = 3.14
+R = 7 / 8
+V = (4 / 3) * pi * R ** 3
+print("Objętość kuli wynosi", V)
