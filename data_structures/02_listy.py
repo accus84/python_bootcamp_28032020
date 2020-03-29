b3618ab770d1808aa9943eca7834a01812bed835
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 16:27:08 +0200

    Dzien 2

--- data_structures/02_listy.py
+++ data_structures/02_listy.py
@@ -6,6 +6,20 @@ elementy.append("cos")
 #zwraca długość listy
 print(len(elementy))
 
-while len(elementy < 11:
-    elementy.append("xx"))
-print(elementy)
+for element in elementy:
+    print(element)
+
+for x in "abc":
+    if x == 'b':
+        break       #break wychodzi z pętli, continue pomija jeden obrót pętli
+    print(x)
+
+for y in "123":
+    for x in "abc":
+        if x == "b":
+            break   #wyjśćie ALE TYLKO Z PĘTLI ZAGNIEŻDŻONEJ
+        print(x + y)
+
+lista = [1, 4, 5, 7]
+for i in lista:
+    print(i)
