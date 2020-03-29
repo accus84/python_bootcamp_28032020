42793cd1fe697c5fdf34308df0a254988fa04878
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 10:30:58 +0200

    Dzien 2

---
+++ 006a_operatory_logiczne.py
@@ -0,0 +1,5 @@
+#czy liczba większa od 10, mniejsza od 15 i podzielna przez 2
+liczba = 11
+print(liczba > 10 and liczba < 15 and liczba % 2 == 0)
+#AND - true jest tylko wtedy gdy wszystkie warunki są spełnione
+#OR - true gdy przynajmniej jeden warunek jest spełniony
--- 06_zadanie.py
+++ 06_zadanie.py
@@ -3,3 +3,10 @@ print()
 print(f"Większa od 10: {liczba > 10}")
 print(f"Mniejsza równa 15: {liczba <= 15}")
 print(f"Podzielna przez 2: {liczba % 2 == 0}")
+
+#lub inaczej printy
+print(f"""
+Większa od 10: {liczba > 10}")
+Mniejsza równa 15: {liczba <= 15}")
+Podzielna przez 2: {liczba % 2 == 0}
+""")
