98f16b224fc829a0b4c441a925143e15cfb5efc0
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 13:46:12 +0200

    Dzien 2

--- 13_zadanie.py
+++ 13_zadanie.py
@@ -1,10 +1,10 @@
 #zapytać o 7 temperatur, policzyć średnią
-dni = 1
+day = 1
 temp_total = 0
-while dni < 8:
-    temp = int(input(f"Podaj temperaturę dla {dni} dnia: "))
+while day < 8:
+    temp = int(input(f"Podaj temperaturę dla {day} dnia: "))
     print(temp)
-    dni +=1
+    day +=1
     temp_total += temp
 print(f"Suma temperatur 7 dni wynosi {int(temp_total / 7)}")
 
