9e6f3ad25d8257ef0daf62cb9d0e89a662ff545b
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 10:33:36 +0200

    Dzien 2

--- 07_zadanie.py
+++ 07_zadanie.py
@@ -1,4 +1,4 @@
 #czy podana liczba jest podzielna przez 2, podzielna przez 3 i większa od 10 lub jest to liczba 7
 liczba = int(input("Podaj liczbę: "))
 print()
-print(f"Czy podana liczba jest podzielna przez 2, podzielna przez 3 i większa od 10 lub jest to liczba 7? {(liczba % 2 == 0 and liczba % 3 == 0) or liczba == 7 }")
+print(f"Czy podana liczba jest podzielna przez 2, podzielna przez 3 i większa od 10 lub jest to liczba 7? {(liczba % 2 == 0 and liczba % 3 == 0 and liczba > 10) or liczba == 7 }")
