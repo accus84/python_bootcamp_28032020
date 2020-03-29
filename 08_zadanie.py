472de2e7999f6faf4c243f6a331da804d2fcad62
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 10:48:40 +0200

    Dzien 2

--- 08_zadanie.py
+++ 08_zadanie.py
@@ -1,9 +1,9 @@
-#Napisz program który na podstawie opakowania obliczy jego objętość oraz sprawdzy czy jest większa od 1 litra (100cm3)
+#Napisz program który na podstawie opakowania obliczy jego objętość oraz sprawdzi czy jest większa od 1 litra (100cm3)
 #V = a * b * h
 dlugosc = int(input("Podaj dlugosc: "))
 szerokosc = int(input("Podaj szerokosc: "))
 wysokosc = int(input("Podaj wysokosc: "))
 print()
 print(f"""
-Czy objętość większa od 1 litra? {dlugosc * szerokosc * wysokosc > 100}   
+Czy objętość większa jest większa od 1 litra? {dlugosc * szerokosc * wysokosc > 100}   
 """)
