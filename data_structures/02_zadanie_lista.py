0cf5fdbfa99bbef4b99ffe277ddefc36b02a2244
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 15:57:23 +0200

    Dzien 2

--- data_structures/02_zadanie_lista.py
+++ data_structures/02_zadanie_lista.py
@@ -1,10 +1,8 @@
 #Napisz program obliczający średnią wartość podanych przez użytkownika liczb
 # Pozwól na wprowadzenie max 10 liczb. Skorzystaj z funkcji sum()
 tablica = []
-licznik = 1
-while licznik < 11:
-    element = int(input(f"Podaj liczbę numer {licznik}: "))
+while len(tablica) < 10:
+    element = int(input(f"Podaj liczbę: "))
     tablica.append(element)
-    licznik += 1
 print(f"Suma z {len(tablica)} liczb wynosi {sum(tablica)}")
 print(f"Średnia z {len(tablica)} liczb wynosi {sum(tablica) / len(tablica)}")
