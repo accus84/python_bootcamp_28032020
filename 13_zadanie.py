14143758ad5ede667cb83e56de4507ceb6b0b8bb
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 13:51:40 +0200

    Dzien 2

--- 13_zadanie.py
+++ 13_zadanie.py
@@ -3,7 +3,6 @@ day = 1
 temp_total = 0
 while day < 8:
     temp = int(input(f"Podaj temperaturę dla {day} dnia: "))
-    print(temp)
     day +=1
     temp_total += temp
 print(f"Średnia temperatur dla 7 dni wynosi {round((temp_total / 7), 2)}")
