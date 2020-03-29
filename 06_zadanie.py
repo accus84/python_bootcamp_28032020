e6142ca8f273f148f51e66074b2e82e8c556f322
Artur Żołyński <a.zolynski@visiona.pl>
2020-03-29 10:16:08 +0200

    Dzien 2

--- 06_zadanie.py
+++ 06_zadanie.py
@@ -1,4 +1,5 @@
 liczba = int(input("Podaj liczbę: "))
+print()
 print(f"Większa od 10: {liczba > 10}")
 print(f"Mniejsza równa 15: {liczba <= 15}")
-print(f"podzielna przez 2: {liczba % 2 == 0}")
+print(f"Podzielna przez 2: {liczba % 2 == 0}")
